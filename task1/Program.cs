5e643ec3c527a9664e7e823991dada345a81682e
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-22 22:34:22 +0300

    Решили первую задачу

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// является ли второе число квадратом первого
+Console.WriteLine("введите число");
+int number1 = int.Parse(Console.ReadLine() ??"0");
+Console.WriteLine("введите квадрат этого числа");
+int number2 = int.Parse(Console.ReadLine() ??"0");
+
+if (number2 == (number1 * number1))
+{
+Console.Write(" Гениально! ");
+}
+else 
+{
+  Console.Write("Хм, тебе придется как следует подумать, над этой задачкой.");  
+}

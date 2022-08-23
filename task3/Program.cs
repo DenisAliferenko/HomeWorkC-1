21d1149faf8fe6c0f76400c2b00c056a136e5c1d
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-23 07:40:51 +0300

    решили 3 задачу

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите три числа, сравним какое больше");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int z = int.Parse(Console.ReadLine());
+          
+int max = 0;
+{ 
+if (x >= y && x >= z) max = x;
+else if (y >= x && y >=z ) max = y;
+else if (z >= x && z >= y) max = z;
+}
+Console.WriteLine("Максимальное число {0}", max);

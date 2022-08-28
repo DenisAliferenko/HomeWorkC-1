725f1c5851de7bd5137e9e3edd26ecf2a15e251f
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-28 22:14:57 +0300

    Проверка числа четное или не четное

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ??"0");
+
+if (number % 2 == 1)
+{
+    Console.WriteLine($"Не четное");
+}
+else 
+{
+    Console.WriteLine($"Четное");
+}

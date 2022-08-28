13f5c5fb8fff56ff58e9cbe6c2b1cfdd85dd97f8
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-29 00:01:34 +0300

    вторая цифра трехзначного числа

---
+++ task6/Program.cs
@@ -0,0 +1,6 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int n = int.Parse(Console.ReadLine());
+int result = n % 100;
+int res = result / 10;
+Console.WriteLine(res);

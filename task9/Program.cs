36a4377a5128eb80c251a043ecc238322e7d2395
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-29 22:51:40 +0300

    Выяснить кратно ли число заданному значению, если нет вывести остаток

---
+++ task9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число кратное заданному значению: ");
+double n = double.Parse(Console.ReadLine());
+double x = 7;
+double z = 0;
+
+if (n % x == 0)
+{
+Console.Write($"Угадал! Твое число кратно {x}. ");
+}
+else 
+{
+    Console.Write($"остаток от деления {n % x} ");  
+}

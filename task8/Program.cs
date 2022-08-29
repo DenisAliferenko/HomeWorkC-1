84e0f48898900c7aa913813b3bc4e0dc8f8f7e07
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-29 22:08:58 +0300

    удалить вторую цифру трехзначного числа

---
+++ task8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = number / 100;
+int res = result % 10;
+
+int rx = number % 10;
+
+Console.WriteLine($"{res}{rx}");

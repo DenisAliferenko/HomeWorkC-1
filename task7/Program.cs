0bb22223bf50280208221253ed04e742721a833c
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-08-29 21:42:01 +0300

    в отрезе от 10 до 99 дано одно число. показать наибольшую цифру данного числа

---
+++ task7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Clear();
+int number = new Random().Next(10, 100);
+int max = 0;
+
+int MaxDigit()
+{
+    int DigitOne = number / 10;
+    int DigitTwo = number % 10;
+    if (DigitOne > DigitTwo) max = DigitOne;
+    else max = DigitTwo;
+
+    return max;
+}
+
+MaxDigit();
+Console.WriteLine();
+Console.WriteLine($"В числе {number} наибольшая цифра {max}");
+Console.WriteLine();

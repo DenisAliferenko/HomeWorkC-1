f7c8e76fa29b7374fd537455996e6a0e9daf9d2d
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-01 21:38:34 +0300

    по двум заданным числам проверять является ли одно квадратом дургого

---
+++ Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Введи число квадрат которого ищем");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи число которое является квадратом первого");
+int x = int.Parse(Console.ReadLine());
+
+if (n * n == x)
+{
+    Console.WriteLine($"Верно{x} является квадратом {n}");
+}
+else 
+{
+    Console.WriteLine($"Ошибка! Квадратом {n} является {n*n} ");
+}

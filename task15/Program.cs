409afbe4c71a5e989b55ba9a4d7a67a8afc35dd9
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-04 15:47:44 +0300

    найти кубы чисел от 1 до N

---
+++ task15/Program.cs
@@ -0,0 +1,32 @@
+﻿// Найти кубы чисел от 1 до N
+// Convert.ToInt32 - переобразует любое введеное знаечение в тип int
+// Math.Pow - использется для возведения числа в степень
+Console.Write("Введите последнюю цифру отрезка чисел к которым будем искать кубы: ");
+int cube = int.Parse(Console.ReadLine() ?? "0");
+
+void Cube(int[] cube)
+{
+  int i = 0;
+  while (i < cube.Length)
+  {
+        cube[i] = i * i * i;
+        i++;
+  }
+}
+
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  while(index < count)
+
+  {
+    Console.WriteLine(coll[index] + " ");
+    index++;
+  }
+
+} 
+
+int[] arry = new int[cube + 1];
+Cube(arry);
+PrintArry(arry);

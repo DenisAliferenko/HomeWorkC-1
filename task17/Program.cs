6759b026b1fcc60d368d4a5ce9460945e52930f3
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-04 21:30:08 +0300

    подсчет цифр в числе

---
+++ task17/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ??"0");
+int digits = 0; // кол-во цифр
+
+while (number > 0)
+{
+    digits++;
+    number = number / 10;
+}
+Console.WriteLine($"в числе {digits} цифр(ы) ");

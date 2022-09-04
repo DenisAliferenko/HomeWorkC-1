52781a3fd1327d8053884e65510b68afd894835b
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-04 16:55:40 +0300

    Возведение числа в натуральную степень через цикл

---
+++ Task16/Program.cs
@@ -0,0 +1,18 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число которое будем возводить в степень");
+int number = int.Parse(Console.ReadLine() ??"0");
+Console.WriteLine("укажите в какую натуральную степень возводим: ");
+int degree =int.Parse(Console.ReadLine() ??"0");
+
+int count = 1;
+double result = number;
+
+while (count < degree)
+{
+    result = result * number;
+    count++;
+}
+
+Console.WriteLine($"{number} ^ {degree} = {result}");
+Console.WriteLine();

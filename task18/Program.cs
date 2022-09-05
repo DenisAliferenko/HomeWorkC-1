b552a74e4c227073eb717e0f236158bcca1fcdb0
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-05 21:25:26 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ task18/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Написать программу вычисления произведения чисел от 1 до N
+Console.Clear();
+Console.Write(" Напишем произведения чисел от 1 до:  ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+int res = 1;
+
+for (int num = 1; num <= x; num++)
+    {   
+        res = res * num;
+        Console.WriteLine(res);
+        
+    }
+return res;
+
+//  подскажиет как написть с double ?? после 16 цифры выдает отрицательные числа. При попытке преорбразовать в double не работает return res;

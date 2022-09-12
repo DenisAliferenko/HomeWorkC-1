e222c245b426742ff5476e56594d9de122f048ce
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-12 23:10:40 +0300

    В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]

---
+++ task22/Program.cs
@@ -0,0 +1,26 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+void FillArray(int[] chisla)
+{
+    int length = chisla.Length;
+    for(int i = 0; i < length; i++)
+    {
+     chisla[i] = new Random().Next(1,1000);
+    }
+}
+int[] array = new int[15];
+
+void binarinumbers(int[] arr)
+{
+    int binare = 0;
+    for (int count = 0; count < arr.Length; count++)
+    {
+        if (arr[count] >= 10 && arr[count] <=99) binare++;
+        Console.Write(arr[count] + " ");
+        
+    }
+    Console.WriteLine();
+    Console.WriteLine($" Элементов из отрезка [10,99] = {binare}");
+}
+
+FillArray(array);
+binarinumbers(array);

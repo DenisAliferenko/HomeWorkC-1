b9704ee4b6ae5faeac371fa5c6f45f2a9d2e0fbc
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-15 00:50:34 +0300

    В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом

---
+++ task24/Program.cs
@@ -0,0 +1,26 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+Console.WriteLine("В массиве из вещественных чисел: ");
+double[] array = new double[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().NextDouble() * 100;
+    Console.WriteLine(array[i] + " ");
+}
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    {
+        if (max < array[i]) max = array[i];
+    }
+    {
+        if (min > array[i]) min = array[i];
+    }
+}
+double difference = max - min;
+
+Console.WriteLine($"\nразница между между максимальным ({max}) и минимальным({min}) элементами: {difference}");
+
+

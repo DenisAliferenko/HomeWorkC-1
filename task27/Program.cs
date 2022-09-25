6e4773324c1eb8d25e8e96f7f2c505da786fbfd7
DenisAliferenko <110713090+DenisAliferenko@users.noreply.github.com>
2022-09-25 22:41:08 +0300

    Показать двумерный массив размером mn заполненный вещественными числами

---
+++ task27/Program.cs
@@ -0,0 +1,32 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+Console.WriteLine("Задайте размер двумерного массива");
+int m = int.Parse(Console.ReadLine() ??"0");
+int n = int.Parse(Console.ReadLine() ??"0");
+
+void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++ )
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+       Console.Write($" {matr [i, j]} ");
+    }
+Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+    for(int i =0; i < matr.GetLength(0); i++)
+    {
+        for(int j =0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+int[,] matrix1 = new int[m, n];
+FillArray(matrix1);
+Console.WriteLine();
+
+PrintArray(matrix1);
